17bdc2877c9b05b385a7ec22a6a0dca93e0cc29c
kasztelmacko <kasztelanicmaciej@gmail.com>
2025-05-29 17:18:47 +0000

    main script clear

--- main.R
+++ main.R
@@ -1,15 +1,9 @@
 # NOTE: Add new installs and imports to the project_setup.R file:
 source('project_setup.R')
 source('data/download_data.R')
+source('data/data_loader_classes.R')
 download_split("val")
 
-source('data/data_loader_classes.R')
-source('model/attention_classes.R')
-
-source('model/gpt_model.R')
-
-source("eval/inference_test.R")
-source("eval/hellaswag_test.R")
 
 # Set seeds
 torch_manual_seed(1337)
@@ -23,7 +17,6 @@ enc <- tiktoken$get_encoding("gpt2")
 # -----------------------------------------------------------------------------
 # Load Weights and Test Inference
 
-# Define the path to your saved weights file
 weights_path <- "weights/Model-weights.pt"
 
 if (!file.exists(weights_path)) {
@@ -31,7 +24,6 @@ if (!file.exists(weights_path)) {
 }
 message(sprintf("Loading weights from %s", weights_path))
 
-# Load the checkpoint
 checkpoint <- torch_load(weights_path)
 original_weights <- checkpoint
 
@@ -45,9 +37,10 @@ if (!is.null(checkpoint$config)) {
 }
 
 # Instantiate model
+source('model/attention_classes.R')
+source('model/gpt_model.R')
 test_model <- GPT(config = loaded_config)$to(device = loaded_config$device)
 
-# load and process weights
 source('model/load_weights.R')
 
 # Set the model to evaluation mode
@@ -55,6 +48,7 @@ test_model$eval()
 
 # -----------------------------------------------------------------------------
 # Check Inference
+# source("eval/inference_test.R")
 # prompt_text <- "Hello, I'm Mike,"
 # max_length <- 32 # Maximum length of generated text
 # num_return_sequences <- 4 # Number of sequences to generate
@@ -63,4 +57,5 @@ test_model$eval()
 
 # -----------------------------------------------------------------------------
 # Hellaswag Test
+source("eval/hellaswag_test.R")
 hellaswag_test(test_model, enc, loaded_config)
